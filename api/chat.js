fb82dbf9429231bc5ab98af9d4bed062c929342c
JoanathanPS <joanathanps2006@gmail.com>
2025-12-06 00:04:05 +0530

    Fix Vercel serverless function export format

--- api/chat.js
+++ api/chat.js
@@ -23,7 +23,7 @@ module.exports = async (req, res) => {
   }
 
   if (req.method !== 'POST') {
-    return res.status(405).json({ success: false, message: 'Method not allowed' });
+    return res.status(405).json({ success: false, message: `Method ${req.method} not allowed. Use POST.` });
   }
 
   try {
